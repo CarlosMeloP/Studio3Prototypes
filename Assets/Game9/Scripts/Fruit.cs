0669ceb2c44516c75016af5008d70f98b7d4cf6e
CarlosMeloStuff <carlosmelo1991@gmail.com>
2016-10-04 11:22:35 +1000

    Game 9

---
+++ Assets/Game9/Scripts/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit : MonoBehaviour {
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BOARD"))
+        {
+            var collisionPos = collision.contacts[0].point;
+
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Game9/Scripts/FruitSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    public static FruitSpawner Instance;
+    public GameObject FruitPrefab;
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject SpawnFruit()
+    {
+        var newFruit = Instantiate(FruitPrefab);
+
+
+        return newFruit;
+    }
+}
